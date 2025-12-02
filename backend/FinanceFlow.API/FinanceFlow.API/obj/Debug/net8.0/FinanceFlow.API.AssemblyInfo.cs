01f1fd0ded481fac9e66bf42e3122ff765b256ee
FuraCse <lakatoscsongor2@gmail.com>
2025-12-02 12:44:17 +0100

    verz1

--- backend/FinanceFlow.API/FinanceFlow.API/obj/Debug/net8.0/FinanceFlow.API.AssemblyInfo.cs
+++ backend/FinanceFlow.API/FinanceFlow.API/obj/Debug/net8.0/FinanceFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bfade8ae5fbe14c926e682da6b8894d0fd1f491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11db7dd39923923ad12505ac5bcafabee84bdb4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

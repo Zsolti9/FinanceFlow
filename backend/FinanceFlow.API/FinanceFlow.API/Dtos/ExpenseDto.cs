fbc4bc1f79d27151ea9921732ecd32ddf7237033
FuraCse <lakatoscsongor2@gmail.com>
2026-01-21 11:05:14 +0100

    ne bazd el zsoli

---
+++ backend/FinanceFlow.API/FinanceFlow.API/Dtos/ExpenseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceFlow.API.Dtos
+{
+    public class ExpenseDto
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ backend/FinanceFlow.API/FinanceFlow.API/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceFlow.API.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
